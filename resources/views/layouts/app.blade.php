8753db26d331899d4004dfff4dab90574ac6da4d
ijunnn <faizunaziz53@gmail.com>
2025-06-19 13:24:38 +0700

    Update : App Layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -607,7 +607,7 @@
                         </div>
                         <div class="col-sm-6">
                             <div class="text-sm-end d-none d-sm-block">
-                                Design & Develop by Themesbrand
+                                Design & Develop by SwaraningCode
                             </div>
                         </div>
                     </div>
